87b6bbbb212c6db49ff601f4442112c67496888f
elliesimens <elliesimens@gmail.com>
2024-10-17 03:18:49 -0400

    fixed some bugs

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 let focusTabId = null;
-//let isFocused = true; ///???
 
 function injectContentScript(tabId) {
   return chrome.scripting.executeScript({
@@ -11,13 +10,17 @@ function injectContentScript(tabId) {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'focusHere') {
     focusTabId = message.tabId;
-    injectContentScript(focusTabId);
-    sendResponse({ status: 'Successfully set focus tab' });
+    injectContentScript(focusTabId).then(() => {
+      chrome.tabs.sendMessage(focusTabId, {
+        type: 'judgeUser',
+        isFocused: true,
+      });
+      sendResponse({ status: 'Successfully set focus tab' });
+    });
   }
 });
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
-  //this is how you listen for being on the focus tab
   changeSmiley(activeInfo.tabId);
 });
 
@@ -27,32 +30,10 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 });
 
-// Function to handle tab activation and send appropriate messages
 function changeSmiley(activeTabId) {
-  if (focusTabId === null) return; // No focus tab set
-
-  // Inject content script into the activated tab
+  if (focusTabId === null) return;
   injectContentScript(activeTabId).then(() => {
-    // Determine if the activated tab is the focus tab
     const isFocused = activeTabId === focusTabId;
-
-    // Send a message to the activated tab to display smiley or angry face
-    chrome.tabs.sendMessage(
-      activeTabId,
-      { type: 'judgeUser', isFocused },
-      (response) => {
-        if (chrome.runtime.lastError) {
-          console.error(
-            'Error sending message to content script:',
-            chrome.runtime.lastError
-          );
-          // Optionally, you can attempt to re-inject the content script and resend the message
-        } else {
-          console.log(
-            `Sent message to tab ${activeTabId}: isFocused = ${isFocused}`
-          );
-        }
-      }
-    );
+    chrome.tabs.sendMessage(activeTabId, { type: 'judgeUser', isFocused });
   });
 }
